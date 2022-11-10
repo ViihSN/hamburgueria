28129f1035b993581dfbf71d5eb7345250283954
Gustavo Arruda <70242475+gugalxp@users.noreply.github.com>
2022-11-10 02:07:24 -0300

    Ajuste menu

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,20 +1,35 @@
 import "./navbar.css";
+import { Link } from "react";
 import Logo from "../../assets/images/logo2.png";
+import { FaInstagramSquare, FaFacebookSquare, FaWhatsappSquare } from 'react-icons/fa';
 
 export default function Navbar() {
 
     return (
 
         <div className="container-navbar">
-            
-            <img src={Logo} className="logo animate__animated animate__bounce" alt=""/>
-
+            <div className="top-navbar">
+                <img src={Logo} className="logo animate__animated animate__bounce" alt=""/>
+                <div>
+                    <FaInstagramSquare className="icons"/>
+                    <FaFacebookSquare className="icons"/>
+                    <FaWhatsappSquare className="icons"/>
+                </div>
+            </div>
             <div className="container-navbar-itens">
                 <ul>
-                    <li>Contato</li>
-                    <li>Quem Somos</li>
-                    <li>Localização</li>
-                    <li>Lanches</li>
+                    <li>
+                        <a href="">Contato</a> 
+                    </li>
+                    <li>
+                        <a href="">Quem Somos</a> 
+                    </li>
+                    <li>
+                        <a href="">Localização</a> 
+                    </li>
+                    <li>
+                        <a href="">Lanches</a> 
+                    </li>
                 </ul>
             </div>
         </div>
