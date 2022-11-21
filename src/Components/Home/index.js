abe179044bf248560dbb6ef1df943d087ef95224
Gustavo Arruda <70242475+gugalxp@users.noreply.github.com>
2022-11-21 02:55:48 -0300

    Separação dos componentes e ajuste de design no home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,3 +1,39 @@
 import "./home.css";
+import Chicken from "../../assets/images/HamburguerChicken.png";
+import Thumb1 from "../../assets/images/Thumb1.png";
+import Thumb2 from "../../assets/images/Thumb2.png";
+import Thumb3 from "../../assets/images/Thumb3.png";
+import { FaInstagramSquare, FaFacebookSquare, FaWhatsappSquare } from 'react-icons/fa';
 
-export default function Home() {}
+export default function Home() {
+
+    return (
+        <div className="home">
+            <div class="circle"></div>
+            <div class="content">
+                <div class="textBox">
+                    <h2>Não é só Hamburguer Artesanal<br/>É <span>V-Burger's!</span></h2>
+                    <p>Não passe vontade, peça já o seu hambúrguer.</p>
+                    <a href="#" className="button">Aqui!!!</a>
+                </div>
+            </div>
+            <div class="imgBox">
+                <img src={Chicken} class="hamburgueres"/>
+            </div>
+            <ul class="thumb">
+                <li>
+                    <img src={Thumb1} alt="..."/>
+                    <img src={Thumb2} alt="..."/>
+                    <img src={Thumb3} alt="..."/>
+                </li>
+            </ul>
+            <div class="icone-rede-sociais">
+                <ul >
+                    <li><a href="#"><FaInstagramSquare className="icons"/></a></li>
+                    <li><a href="#"><FaFacebookSquare className="icons"/></a></li>
+                    <li><a href="#"><FaWhatsappSquare className="icons"/></a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,19 +1,12 @@
 import "./navbar.css";
 import Logo from "../../assets/images/logo2.jpg";
-import Chicken from "../../assets/images/HamburguerChicken.png";
-import Thumb1 from "../../assets/images/Thumb1.png";
-import Thumb2 from "../../assets/images/Thumb2.png";
-import Thumb3 from "../../assets/images/Thumb3.png";
-import { FaInstagramSquare, FaFacebookSquare, FaWhatsappSquare } from 'react-icons/fa';
-
 
 
 export default function Navbar() {
 
     return (
         
-        <section>
-            <div class="circle"></div>
+        <>
             <header>
                 <a href="#">
                   <img src={Logo} class="logo" alt="..."/>
@@ -25,30 +18,6 @@ export default function Navbar() {
                     <li><a href="#">Endereço</a></li>
                 </ul>
             </header>
-            <div class="content">
-                <div class="textBox">
-                    <h2>Não é só Hamburguer Artesanal<br/>É <span>V-Burger's!</span></h2>
-                    <p>Não passe vontade, peça já o seu hambúrguer.</p>
-                    <a href="#" class="button">Aqui!!!</a>
-                </div>
-            </div>
-            <div class="imgBox">
-                <img src={Chicken} class="hamburgueres"/>
-            </div>
-            <ul class="thumb">
-                <li>
-                    <img src={Thumb1} alt="..."/>
-                    <img src={Thumb2} alt="..."/>
-                    <img src={Thumb3} alt="..."/>
-                </li>
-            </ul>
-            <div class="icone-rede-sociais">
-                <ul >
-                    <li><a href="#"><FaInstagramSquare className="icons"/></a></li>
-                    <li><a href="#"><FaFacebookSquare className="icons"/></a></li>
-                    <li><a href="#"><FaWhatsappSquare className="icons"/></a></li>
-                </ul>
-            </div>
-        </section>
+        </>
     )
 }
