1f1a896c5fd4ea7761154bf306b539e42f255bfd
vitoriasn <vsn.gja@gmail.com>
2023-01-12 01:58:41 -0300

    Construção dos cards de lanches

--- src/Components/Section1/index.js
+++ src/Components/Section1/index.js
@@ -1,60 +1,134 @@
 import "./section1.css";
-import Bacon from "../../assets/images/img-lanches/foto1.png";
-import Cogumelo from "../../assets/images/img-lanches/foto2.png";
-import Malagueta from "../../assets/images/img-lanches/foto3.png";
+import Bacon from "../../assets/images/img-lanches/lanche1.png";
+import Cogumelo from "../../assets/images/img-lanches/lanche2.png";
+import Malagueta from "../../assets/images/img-lanches/lanche3.png";
+import Vegano from "../../assets/images/img-lanches/lanche4.png";
+import Chicken from "../../assets/images/img-lanches/lanche5.png";
+import Kids from "../../assets/images/img-lanches/lanche6.png";
 import { BiFoodMenu } from 'react-icons/bi';
 
 
 export default function Section1() {
 
+    
     return(
         <section id="sessao1">
             <div className="text-center">
                 <h1><BiFoodMenu color="red"/>Cardápio</h1>
             </div>
         
-        <div className="row d-flex justify-content-center">
-            <div className="card" onClick={() => this.classList.toggle('expanded')} alt="...">
-               <img src={Bacon}/>
-               <div className="text1">
-                    <div className="text-content">
-                     <h1 className="title">V-BACON</h1>
-                    <div className="body-text">
-                        Ingredientes: <br/>Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                        
+            <div className="container">
+                <div className="card">
+                    <div className="imgBx">
+                        <img src={Bacon}/>
                     </div>
-                  </div>
-               </div>
-               <svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 35" width="30"><path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5"/></svg>
-            </div>
-            <div className="card" onClick={() => this.classList.toggle('expanded')} alt="...">
-               <img src={Cogumelo}/>
-               <div className="text1">
-                    <div className="text-content">
-                     <h1 className="title">V-COGUMELO</h1>
-                    <div className="body-text">
-                        Ingredientes: <br/>Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                        
+                    <div className="contentBx">
+                        <h2>V-BACON</h2>
+                        <div class="size">
+                            <h3> 
+                             Ingredientes:
+                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
+                            </h3>
+                        </div> 
+                        <div className="dolar">
+                            <h3>R$28,90</h3>
+                        </div>
+                        <a href="#">Peça Aqui!</a>
                     </div>
-                  </div>
-               </div>
-               <svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 35" width="30"><path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5"/></svg>
-            </div>
-            <div className="card" onClick={() => this.classList.toggle('expanded')} alt="...">
-               <img src={Malagueta}/>
-               <div className="text1">
-                    <div className="text-content">
-                     <h1 className="title">V-MALAGUETA</h1>
-                    <div className="body-text">
-                        Ingredientes: <br/>Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                        
+                </div>
+                <div className="card">
+                    <div className="imgBx">
+                        <img src={Malagueta}/>
                     </div>
-                  </div>
-               </div>
-               <svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 35" width="30"><path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5"/></svg>
+                    <div className="contentBx">
+                        <h2>V-MALAGUETA</h2>
+                        <div class="size">
+                            <h3> 
+                             Ingredientes:
+                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
+                            </h3>
+                        </div> 
+                        <div className="dolar">
+                            <h3>R$28,90</h3>
+                        </div>
+                        <a href="#">Peça Aqui!</a>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="imgBx">
+                        <img src={Cogumelo}/>
+                    </div>
+                    <div className="contentBx">
+                        <h2>V-COGUMELO</h2>
+                        <div class="size">
+                            <h3> 
+                             Ingredientes:
+                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
+                            </h3>
+                        </div> 
+                        <div className="dolar">
+                            <h3>R$28,90</h3>
+                        </div>
+                        <a href="#">Peça Aqui!</a>
+                    </div>
+                </div>
             </div>
-        </div>
-
+            <div className="container2">
+                <div className="card2">
+                    <div className="imgBx2">
+                        <img src={Chicken}/>
+                    </div>
+                    <div className="contentBx2">
+                        <h2>V-CHICKEN</h2>
+                        <div class="size2">
+                            <h3> 
+                             Ingredientes:
+                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
+                            </h3>
+                        </div> 
+                        <div className="dolar2">
+                            <h3>R$28,90</h3>
+                        </div>
+                        <a href="#">Peça Aqui!</a>
+                    </div>
+                </div>
+                <div className="card2">
+                    <div className="imgBx2">
+                        <img src={Vegano}/>
+                    </div>
+                    <div className="contentBx2">
+                        <h2>V-VEGANO</h2>
+                        <div class="size2">
+                            <h3> 
+                             Ingredientes:
+                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
+                            </h3>
+                        </div> 
+                        <div className="dolar2">
+                            <h3>R$28,90</h3>
+                        </div>
+                        <a href="#">Peça Aqui!</a>
+                    </div>
+                </div>
+                <div className="card2">
+                    <div className="imgBx2">
+                        <img src={Kids}/>
+                    </div>
+                    <div className="contentBx2">
+                        <h2>V-KIDS</h2>
+                        <div class="size2">
+                            <h3> 
+                             Ingredientes:
+                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
+                            </h3>
+                        </div> 
+                        <div className="dolar2">
+                            <h3>R$28,90</h3>
+                        </div>
+                        <a href="#">Peça Aqui!</a>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
