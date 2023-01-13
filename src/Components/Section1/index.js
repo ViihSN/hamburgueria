f9d5d1b445fd48e227cd8154df8911a4c84699c5
vitoriasn <vsn.gja@gmail.com>
2023-01-13 17:01:34 -0300

    Alteração dos cards de lanches

--- src/Components/Section1/index.js
+++ src/Components/Section1/index.js
@@ -10,122 +10,104 @@ import { BiFoodMenu } from 'react-icons/bi';
 
 export default function Section1() {
 
-    
     return(
         <section id="sessao1">
             <div className="text-center">
-                <h1><BiFoodMenu color="red"/>Cardápio</h1>
+                <BiFoodMenu color="red" size="30"/>
+                <h1>Cardápio</h1>
             </div>
-        
-            <div className="container">
-                <div className="card">
-                    <div className="imgBx">
-                        <img src={Bacon}/>
-                    </div>
-                    <div className="contentBx">
-                        <h2>V-BACON</h2>
-                        <div class="size">
-                            <h3> 
-                             Ingredientes:
-                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                            </h3>
+            
+            <div className="dishes" id="dishes" >
+                <div className="box-container">
+                    <div className="box">
+                       <a href="#" className="fas fa-heart"></a>
+                       <a href="#" className="fas fa-eye"></a>
+                       <img src={Bacon} alt=" "/>
+                       <h3>V-BACON</h3>
+                        <div className="stars">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star-half-alt"></i>
                         </div> 
-                        <div className="dolar">
-                            <h3>R$28,90</h3>
+                        <span>R$28,90</span>
+                        <a href="#" className="btn">add to cart</a>
+                    </div>
+                    <div className="box">
+                        <a href="#" className="fas fa-heart"></a>
+                        <a href="#" className="fas fa-eye"></a>
+                        <img src={Cogumelo} alt=" "/>
+                        <h3>V-COGUMELO</h3>
+                        <div className="stars">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star-half-alt"></i>
                         </div>
-                        <a href="#">Peça Aqui!</a>
+                        <span>R$32,00</span>
+                        <a href="#" className="btn">add to cart</a>
                     </div>
-                </div>
-                <div className="card">
-                    <div className="imgBx">
-                        <img src={Malagueta}/>
-                    </div>
-                    <div className="contentBx">
-                        <h2>V-MALAGUETA</h2>
-                        <div class="size">
-                            <h3> 
-                             Ingredientes:
-                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                            </h3>
-                        </div> 
-                        <div className="dolar">
-                            <h3>R$28,90</h3>
+                    <div className="box">
+                        <a href="#" className="fas fa-heart"></a>
+                        <a href="#" className="fas fa-eye"></a>
+                        <img src={Malagueta} alt=" "/>
+                        <h3>V-MALAGUETA</h3>
+                        <div className="stars">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star-half-alt"></i>
                         </div>
-                        <a href="#">Peça Aqui!</a>
+                        <span>R$35,90</span>
+                        <a href="#" className="btn">add to cart</a>
                     </div>
-                </div>
-                <div className="card">
-                    <div className="imgBx">
-                        <img src={Cogumelo}/>
-                    </div>
-                    <div className="contentBx">
-                        <h2>V-COGUMELO</h2>
-                        <div class="size">
-                            <h3> 
-                             Ingredientes:
-                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                            </h3>
-                        </div> 
-                        <div className="dolar">
-                            <h3>R$28,90</h3>
+                    <div className="box">
+                        <a href="#" className="fas fa-heart"></a>
+                        <a href="#" className="fas fa-eye"></a>
+                        <img src={Vegano} alt=" "/>
+                        <h3>V-VEGANO</h3>
+                        <div className="stars">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star-half-alt"></i>
                         </div>
-                        <a href="#">Peça Aqui!</a>
+                        <span>R$40,00</span>
+                        <a href="#" className="btn">add to cart</a>
                     </div>
-                </div>
-            </div>
-            <div className="container2">
-                <div className="card2">
-                    <div className="imgBx2">
-                        <img src={Chicken}/>
-                    </div>
-                    <div className="contentBx2">
-                        <h2>V-CHICKEN</h2>
-                        <div class="size2">
-                            <h3> 
-                             Ingredientes:
-                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                            </h3>
-                        </div> 
-                        <div className="dolar2">
-                            <h3>R$28,90</h3>
+                    <div className="box">
+                        <a href="#" className="fas fa-heart"></a>
+                        <a href="#" className="fas fa-eye"></a>
+                        <img src={Chicken} alt=" "/>
+                        <h3>V-CHICKEN</h3>
+                        <div className="stars">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star-half-alt"></i>
                         </div>
-                        <a href="#">Peça Aqui!</a>
+                        <span>R$29,90</span>
+                        <a href="#" className="btn">add to cart</a>
                     </div>
-                </div>
-                <div className="card2">
-                    <div className="imgBx2">
-                        <img src={Vegano}/>
-                    </div>
-                    <div className="contentBx2">
-                        <h2>V-VEGANO</h2>
-                        <div class="size2">
-                            <h3> 
-                             Ingredientes:
-                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                            </h3>
-                        </div> 
-                        <div className="dolar2">
-                            <h3>R$28,90</h3>
+                    <div className="box">
+                        <a href="#" className="fas fa-heart"></a>
+                        <a href="#" className="fas fa-eye"></a>
+                        <img src={Kids} alt=" "/>
+                        <h3>V-KIDS</h3>
+                        <div className="stars">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star-half-alt"></i>
                         </div>
-                        <a href="#">Peça Aqui!</a>
-                    </div>
-                </div>
-                <div className="card2">
-                    <div className="imgBx2">
-                        <img src={Kids}/>
-                    </div>
-                    <div className="contentBx2">
-                        <h2>V-KIDS</h2>
-                        <div class="size2">
-                            <h3> 
-                             Ingredientes:
-                             Pão-duas Carnes-Bacon-Queijo Cheddar-Picles.
-                            </h3>
-                        </div> 
-                        <div className="dolar2">
-                            <h3>R$28,90</h3>
-                        </div>
-                        <a href="#">Peça Aqui!</a>
+                        <span>R$15.99</span>
+                        <a href="#" className="btn">add to cart</a>
                     </div>
                 </div>
             </div>
