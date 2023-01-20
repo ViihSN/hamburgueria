cd4b08bef3a8330921d736e6eef380e9eab988ce
vitoriasn <vsn.gja@gmail.com>
2023-01-20 02:37:09 -0300

    Mudança dos cards de lanche

--- src/Components/Section1/index.js
+++ src/Components/Section1/index.js
@@ -1,111 +1,152 @@
-import React, { useRef, useState } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import "swiper/css/pagination";
+// import Modal from "react-responsive-modal";
+// import { useState } from "react";
 import "./section1.css";
-import { Pagination } from "swiper";
-import Bacon from "../../assets/images/img-lanches/lanche1.png";
-import Cogumelo from "../../assets/images/img-lanches/lanche2.png";
-import Malagueta from "../../assets/images/img-lanches/lanche3.png";
-import Chicken from "../../assets/images/img-lanches/lanche4.png";
-import Vegano  from "../../assets/images/img-lanches/lanche5.png";
-import Kids from "../../assets/images/img-lanches/lanche6.png";
+import Bacon from "../../assets/images/img-lanches/bacon.jpg";
+import Malagueta from "../../assets/images/img-lanches/malagueta.jpg";
+import Chicken from "../../assets/images/img-lanches/chicken.jpg";
+import Vegano  from "../../assets/images/img-lanches/vegano.jpg";
+import Simples from "../../assets/images/img-lanches/simples.jpg";
+import Alho from "../../assets/images/img-lanches/alho.jpg";
 import { BiFoodMenu } from 'react-icons/bi';
 
 
-
+// Modal.setAppElement('root')
 
 export default function Section1() {
+
+    // const [modalIsOpen, setIsOpen] = useState(false)
+
+    // function handleOpenModal(){
+    //     setIsOpen(true)
+    // }
 
     return(
 
         <section id="sessao1">
-            <div className="text-center">
-                <BiFoodMenu color="red" size="50"/>
-                <h1>Cardápio</h1>
+            <div className="container mt-5">
+                <div className="text-center">
+                    <BiFoodMenu color='red' size="40"/>
+                    <h1>Cardápio</h1>
+                </div>
+
+                <div className="container mt-4 d-flex mb-5">
+                    <div className="row">
+
+                        <div className="col-md-4 mt-5">
+                            <div className="card">
+                                <img src={Bacon} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h2 className="card-title">Lanche</h2>
+                                    <div className="d-flex justify-content-between">
+                                        <p className="card-text">ndnfn dknmfiojfo kofkofkokf knmfijif</p>
+                                        <p className="card-text mr-4 text-success">R$40,00</p>
+                                    </div>
+                                    <a href="#" className="card-link" data-toggle="modal" data-target="#modalId">More</a> 
+
+                                </div>
+
+                            </div>    
+
+                        </div>
+
+                        <div className="col-md-4 mt-5">
+                            <div className="card">
+                                <img src={Malagueta} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h2 className="card-title">Lanche</h2>
+                                    <div className="d-flex justify-content-between">
+                                        <p className="card-text">ndnfn dknmfiojfo kofkofkokf knmfijif</p>
+                                        <p className="card-text mr-4 text-success">R$40,00</p>
+                                    </div>
+                                    <a href="#" className="card-link" data-toggle="modal" data-target="#modalId">More</a> 
+
+                                </div>
+
+                            </div>    
+
+                        </div>
+
+                        <div className="col-md-4 mt-5">
+                            <div className="card">
+                                <img src={Chicken} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h2 className="card-title">Lanche</h2>
+                                    <div className="d-flex justify-content-between">
+                                        <p className='card-text'>ndnfn dknmfiojfo kofkofkokf knmfijif</p>
+                                        <p className='card-text mr-4 text-success'>R$40,00</p>
+                                    </div>
+                                    <a href="#" className="card-link" data-toggle="modal" data-target="#modalId">More</a> 
+
+                                </div>
+
+                            </div>    
+
+                        </div>
+
+                        <div className="col-md-4 mt-5">
+                            <div className="card">
+                                <img src={Vegano} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h2 className="card-title">Lanche</h2>
+                                    <div className="d-flex justify-content-between">
+                                        <p className='card-text'>ndnfn dknmfiojfo kofkofkokf knmfijif</p>
+                                        <p className='card-text mr-4 text-success'>R$40,00</p>
+                                    </div>
+                                    <a href="#" className="card-link" data-toggle="modal" data-target="#modalId">More</a> 
+
+                                </div>
+
+                            </div>    
+
+                        </div>
+
+                        <div className="col-md-4 mt-5">
+                            <div className="card">
+                                <img src={Simples} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h2 className="card-title">Lanche</h2>
+                                    <div className="d-flex justify-content-between">
+                                        <p className='card-text'>ndnfn dknmfiojfo kofkofkokf knmfijif</p>
+                                        <p className='card-text mr-4 text-success'>R$40,00</p>
+                                    </div>
+                                    <a href="#" className="card-link" data-toggle="modal" data-target="#modalId">More</a> 
+
+                                </div>
+
+                            </div>    
+
+                        </div>
+
+                        <div className="col-md-4 mt-5">
+                            <div className="card">
+                                <img src={Alho} className="card-img-top"/>
+                                <div className="card-body">
+                                    <h2 className="card-title">Lanche</h2>
+                                    <div className="d-flex justify-content-between">
+                                        <p className='card-text'>ndnfn dknmfiojfo kofkofkokf knmfijif</p>
+                                        <p className='card-text mr-4 text-success'>R$40,00</p>
+                                    </div>
+                                    <a href="#" className="card-link" data-toggle="modal" data-target="#modalId">More</a> 
+
+                                </div>
+
+                            </div>    
+
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
 
-            <Swiper 
-              grabCursor={true}
-              loop={true}
-              centeredSlides={true}
-              spaceBetween={20}
-              pagination={{
-                el: ".swiper-pagination",
-                clickable: true,
-              }}
-              breakpoints={{
-                0: {
-                  slidesPerView: 1,
-                },
-                700: {
-                  slidesPerView: 2,
-                },
-                1000: {
-                  slidesPerView: 3,
-                },
-              }}
-              modules={[Pagination]}
-              className="mySwiper"
-            >
-
-                <div className="swiper food-slider">
-
-                    <div className="swiper-wrapper">
-
-                        <SwiperSlide>
-                            <div className="swiper-slide slide" data-name="food-1">
-                                <img src={Bacon} alt=""/>
-                                <h3>delicious food</h3>
-                                <div class="price">$49.99</div>
-                            </div>
-                        </SwiperSlide>
-
-                        <SwiperSlide>
-                            <div className="swiper-slide slide" data-name="food-2">
-                                <img src={Cogumelo} alt=""/>
-                                <h3>delicious food</h3>
-                                <div class="price">$49.99</div>
-                            </div>
-                        </SwiperSlide>
-
-                        <SwiperSlide>
-                        <div className="swiper-slide slide" data-name="food-3">
-                            <img src={Malagueta} alt=""/>
-                            <h3>delicious food</h3>
-                            <div class="price">$49.99</div>
-                        </div>
-                        </SwiperSlide>
-
-                        <SwiperSlide>
-                            <div className="swiper-slide slide" data-name="food-4">
-                                <img src={Chicken} alt=""/>
-                                <h3>delicious food</h3>
-                                <div class="price">$49.99</div>
-                            </div>
-                        </SwiperSlide>
-
-                        <SwiperSlide>
-                            <div className="swiper-slide slide" data-name="food-5">
-                                <img src={Vegano} alt=""/>
-                                <h3>delicious food</h3>
-                                <div class="price">$49.99</div>
-                            </div>
-                        </SwiperSlide>
-
-                        <SwiperSlide>
-                            <div className="swiper-slide slide" data-name="food-6">
-                                <img src={Kids} alt=""/>
-                                <h3>delicious food</h3>
-                                <div class="price">$49.99</div>
-                            </div>
-                        </SwiperSlide>
-                        
-                    </div>
-                    <div className="swiper-pagination"></div>
-                </div>
-            </Swiper>
+            <div className="modal" id="modalId" >
+                <button className="modal-button">
+                    Modal
+                </button>
+            </div>
+            
+            
+                
         </section>
-    )
+    );
 }
-
